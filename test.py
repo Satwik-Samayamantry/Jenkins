7029218d2559d1eeaddfa3acff9088d8b27542fd
Satwik-Samayamantry <85022024+Satwik-Samayamantry@users.noreply.github.com>
2021-11-22 19:15:07 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,18 @@ import unittest
 from code import greaterthan
 
 class TestGreaterThan(unittest.TestCase):
-    def test_list_int(self):
-       
+    def test1(self):
         a = 3
         b = 2
         result = greaterthan(a,b)
         self.assertEqual(result, 1)
+        
+    def test1(self):
+        a = 2
+        b = 3
+        result = greaterthan(a,b)
+        self.assertEqual(result, 0)
+
 
 if __name__ == '__main__':
     unittest.main()
