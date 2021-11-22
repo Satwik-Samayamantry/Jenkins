c261fe6d31352de76bf526c23c03b41e1e0b61f7
Satwik-Samayamantry <85022024+Satwik-Samayamantry@users.noreply.github.com>
2021-11-22 19:08:01 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,11 +6,10 @@ from code import greaterthan
 
 class TestGreaterThan(unittest.TestCase):
     def test_list_int(self):
-        """
-        Test case to add two numbers
-        """
-        data = [3, 2]
-        result = greaterthan(data)
+       
+        a = 3
+        b = 2
+        result = greaterthan(a,b)
         self.assertEqual(result, 1)
 
 if __name__ == '__main__':
