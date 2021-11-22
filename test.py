3c4ce9a165d087cb686d8f05976dea46bbbae9b7
Satwik-Samayamantry <85022024+Satwik-Samayamantry@users.noreply.github.com>
2021-11-22 19:15:52 +0530

    Update test.py

--- test.py
+++ test.py
@@ -11,8 +11,14 @@ class TestGreaterThan(unittest.TestCase):
         result = greaterthan(a,b)
         self.assertEqual(result, 1)
         
-    def test1(self):
+    def test2(self):
         a = 2
+        b = 3
+        result = greaterthan(a,b)
+        self.assertEqual(result, 0)
+      
+    def test3(self):
+        a = 3
         b = 3
         result = greaterthan(a,b)
         self.assertEqual(result, 0)
